6af2d23b19e9e53822dca834766d0dbded856b02
David Bayon <dbayon82@googlemail.com>
2014-06-25 17:24:38 +0100

    refactored

--- directory.rb
+++ directory.rb
@@ -1,30 +1,38 @@
 require 'csv' 
 
-def interactive_menu
-	students = []
-	loop do
-		puts "1. Input the students"
-		puts "2. Show the students"
-		puts "9. Exit"
-		selection = gets.chomp
+@students = []
 
-		case selection
-			when "1"
-				students = input_students
-			when "2"
-				print_header
-				printer(students)
-				print_footer(students)
-			when "9"
-				exit
-			else
-				puts "I don't know what you meant, try again please"
-		end
+def print_menu
+	puts "1. Input the students\n2. Show the students\n9. Exit"
+end
+
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def process(selection)
+	case selection
+		when "1"
+			input_students
+		when "2"
+			show_students
+		when "9"
+			exit
+		else
+			puts "I don't know what you meant, try again please"
+	end
+end
+
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
 	end
 end
 
 def input_students
-	students = []
 	print "Please enter the names of the students\nTo finish, just hit return twice\n"
 	# creates empty array
 	date = Time.new
@@ -48,22 +56,22 @@ def input_students
 			print "Please tell us your place of birth\n"
 			birthplace = gets.chomp
 
-			students << {:name => name, :cohort => cohort, :hobby => hobby, :birthplace => birthplace}
+			@students << {:name => name, :cohort => cohort, :hobby => hobby, :birthplace => birthplace}
 
 			CSV.open("cohorts.csv", "ab") do |csv|
 				csv << [name, cohort, hobby, birthplace]
 			end
 
-			print "Now we have  #{students.length} students\n"
+			print "Now we have  #{@students.length} students\n"
 
 			print "Please enter the name of the students\nTo finish, just hit return twice\n"
 			name = gets.chomp
 		end
 		print_header
-		print_footer(students)
+		print_footer
 	end
 	# return array of the students
-	students
+	@students
 end
 
 def print_header
@@ -71,14 +79,14 @@ def print_header
 	print "----------------\n"
 end
 
-def printer(students)
-	cohorts = students.map { |student| student[:cohort]}.uniq
+def print_students_list
+	cohorts = @students.map { |student| student[:cohort]}.uniq
 
 	counter = 0
 
 	cohorts.each do |month|
 
-		temp = students.select { |student| student[:cohort] == month}
+		temp = @students.select { |student| student[:cohort] == month}
 		until counter == temp.length
 			print "#{temp[counter][:name]}".center(20), "(#{temp[counter][:cohort]} cohort)".center(20), "Enjoys: #{temp[counter][:hobby]}".center(20), "Place of birth: #{temp[counter][:birthplace]}\n".center(20)
 			counter += 1
@@ -87,11 +95,11 @@ def printer(students)
 	end
 end
 
-def print_footer(names)
-	if names.length > 1
-		print "Overall, we have #{names.length} great students\n"
+def print_footer
+	if @students.length > 1
+		print "Overall, we have #{@students.length} great students\n"
 	else
-		print "Overall, we have #{names.length} great student\n"
+		print "Overall, we have #{@students.length} great student\n"
 	end
 end
 
